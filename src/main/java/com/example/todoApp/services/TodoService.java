9ced1aaa9552d8982c9469d5246917594e17fc02
xobbit26 <xobbit26@gmail.com>
2020-04-30 20:07:14 +0300

    added validation exception and validate todo model

--- src/main/java/com/example/todoApp/services/TodoService.java
+++ src/main/java/com/example/todoApp/services/TodoService.java
@@ -2,9 +2,12 @@ package com.example.todoApp.services;
 
 import com.example.todoApp.DAL.interfaces.TodoContextInterface;
 import com.example.todoApp.DTOs.TodoItemDTO;
+import com.example.todoApp.exceptions.ValidationException;
 import com.example.todoApp.services.interfaces.TodoServiceInterface;
+import org.springframework.util.StringUtils;
 
 import java.awt.print.Pageable;
+import java.time.Instant;
 import java.util.List;
 
 public class TodoService implements TodoServiceInterface {
@@ -16,9 +19,8 @@ public class TodoService implements TodoServiceInterface {
 
     @Override
     public void create(TodoItemDTO todoItem) {
-        //TODO: validation model
-        //validateModel(todoItem);
-
+        validateModel(todoItem);
+        setTodoCreatedTime(todoItem);
         this.todoContext.create(todoItem);
     }
 
@@ -45,5 +47,21 @@ public class TodoService implements TodoServiceInterface {
     @Override
     public void delete(long id) {
         this.todoContext.delete(id);
+    }
+
+
+    private void validateModel(TodoItemDTO todoItem) {
+        if (todoItem == null) {
+            throw new ValidationException("todo item is null");
+        }
+        if (StringUtils.isEmpty(todoItem.getText())) {
+            throw new ValidationException("todo text is null or empty");
+        }
+    }
+
+    private void setTodoCreatedTime(TodoItemDTO todoItem) {
+        if(todoItem.getCreatedOn() == null){
+            todoItem.setCreatedOn(Instant.now());
+        }
     }
 }
