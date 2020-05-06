bd8b43db1b357819a3a1a21c455cc1f5cd5bcf77
xobbit26 <xobbit26@gmail.com>
2020-05-07 00:21:01 +0300

    added cors policy

--- src/main/java/com/example/todoApp/configuration/ApplicationConfig.java
+++ src/main/java/com/example/todoApp/configuration/ApplicationConfig.java
@@ -6,10 +6,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ApplicationConfiguration {
+public class ApplicationConfig {
 
     @Bean
     public TodoServiceInterface todoService() {
         return new TodoService();
     }
+
 }
