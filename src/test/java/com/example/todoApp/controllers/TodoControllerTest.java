df54281467d8134e4e7e992f37626fdc7ff227d5
xobbit26 <xobbit26@gmail.com>
2020-05-19 00:19:57 +0300

    Created a test

--- src/test/java/com/example/todoApp/controllers/TodoControllerTest.java
+++ src/test/java/com/example/todoApp/controllers/TodoControllerTest.java
@@ -13,9 +13,14 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import javax.transaction.Transactional;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -34,21 +39,38 @@ public class TodoControllerTest {
 
     @Test
     @DisplayName("crate todo item")
-    void CreateTodoItem() throws Exception {
+    @Transactional
+    void createTodoItem() throws Exception {
         long todoItemId = 1;
         Instant cratedOn = Instant.now();
 
         TodoItemDTO expectedTodoItem = new TodoItemDTO(todoItemId, "String", cratedOn, null, false, null);
         todoService.create(expectedTodoItem);
 
-        mvc.perform(get("/api/todo/todo/" + todoItemId)
+        mvc.perform(get("/api/todo/todo/{0}", todoItemId)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id", is((int) expectedTodoItem.getId())))
                 .andExpect(jsonPath("$.text", is(expectedTodoItem.getText())))
                 .andExpect(jsonPath("$.createdOn", is(expectedTodoItem.getCreatedOn().toString())))
                 .andExpect(jsonPath("$.updatedOn", is(expectedTodoItem.getUpdatedOn())))
                 .andExpect(jsonPath("$.completed", is(expectedTodoItem.isCompleted())));
+    }
+
+    @Test
+    @DisplayName("get todo list")
+    @Transactional
+    void getTodoList() throws Exception {
+        int todoItemsSize = 10;
+
+        for (int i = 1; i <= 10; i++) {
+            todoService.create(new TodoItemDTO(i, "String", Instant.now(), null, false, null));
+        }
+
+        mvc.perform(get("/api/todo/todo-list")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(todoItemsSize)));
     }
 }
