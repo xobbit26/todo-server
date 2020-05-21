113de24f81a8346d7cc4b103a7ce448b679437ce
xobbit26 <xobbit26@gmail.com>
2020-05-22 00:37:50 +0300

    Changed application name

--- src/main/java/com/example/todoApp/TodoApplication.java
+++ src/main/java/com/example/todoApp/TodoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodoAppApplication {
+public class TodoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TodoAppApplication.class, args);
+		SpringApplication.run(TodoApplication.class, args);
 	}
 
 }
